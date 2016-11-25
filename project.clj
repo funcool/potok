7a175fe33647f6fbaa44cfcc69fc2546d49d7f05
Andrey Antukh <niwi@niwi.nz>
2016-11-25 21:34:18 +0100

    Add project.clj and tests.

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject funcool/potok "1.0.5"
+  :description "Reactive streams based state management toolkit for ClojureScript"
+  :url "https://github.com/funcool/potok"
+  :license {:name "BSD (2-Clause)"
+            :url "http://opensource.org/licenses/BSD-2-Clause"}
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.229" :scope "provided"]
+                 [funcool/beicon "2.3.0"]]
+  :deploy-repositories {"releases" :clojars
+                        "snapshots" :clojars}
+  :source-paths ["src" "assets"]
+  :test-paths ["test"]
+  :jar-exclusions [#"\.swp|\.swo|user.clj"]
+  :plugins [[lein-ancient "0.6.10"]])
---
+++ test/potok/tests.cljs
@@ -0,0 +1,35 @@
+(ns potok.tests
+  (:require [cljs.test :as t :include-macros true]
+            [beicon.core :as rx]
+            [potok.core :as ptk]))
+
+(enable-console-print!)
+
+(defrecord IncrementBy [n]
+  ptk/UpdateEvent
+  (update [_ state]
+    (update state :counter + n)))
+
+(defrecord AsyncIncrementBy [n]
+  ptk/WatchEvent
+  (watch [_ state stream]
+    (rx/of (->IncrementBy n))))
+
+(t/deftest synchronous-state-transformation-test
+  (t/async done
+    (let [store (ptk/store {:state {:counter 0}})]
+      (-> (rx/take 1 store)
+          (rx/subscribe (fn [{:keys [counter]}]
+                          (t/is (= counter 1)))
+                        nil
+                        done))
+
+      (ptk/emit! store (->IncrementBy 1)))))
+
+(set! *main-cli-fn* #(t/run-tests))
+
+(defmethod t/report [:cljs.test/default :end-run-tests]
+  [m]
+  (if (t/successful? m)
+    (set! (.-exitCode js/process) 0)
+    (set! (.-exitCode js/process) 1)))
