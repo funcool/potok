3cd918879406b037304481e6a49127cb9829b3b0
Andrey Antukh <niwi@niwi.nz>
2016-12-08 22:57:04 +0100

    Set version to 1.1.0.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject funcool/potok "1.0.0"
+(defproject funcool/potok "1.1.0"
   :description "Reactive streams based state management toolkit for ClojureScript"
   :url "https://github.com/funcool/potok"
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.9.293" :scope "provided"]
-                 [funcool/beicon "2.5.0"]]
+                 [funcool/beicon "2.6.0"]]
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
   :source-paths ["src" "assets"]
