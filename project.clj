ca237acc6fa8baac68db72476da35791fb026e9f
Josef Pospíšil <josef.pospisil@laststar.eu>
2017-08-02 15:56:31 +0200

    Upgrade clojurescript and beicon dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.495" :scope "provided"]
-                 [funcool/beicon "3.2.0"]]
+                 [org.clojure/clojurescript "1.9.854" :scope "provided"]
+                 [funcool/beicon "4.0.0"]]
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
   :source-paths ["src" "assets"]
